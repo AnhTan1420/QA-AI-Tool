237d585ee93b66bd9c6dbf9fb4c78f9bae5ae767
Lê Văn Anh Tân <58280404+AnhTan1420@users.noreply.github.com>
2026-08-07 11:08:58 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['playwright-core', '@sparticuz/chromium-min'],
+  serverExternalPackages: ['playwright-core', '@sparticuz/chromium'],
   // Đảm bảo Vercel copy đúng file từ node_modules vào deployment
   outputFileTracingIncludes: {
-    '/api/automation/inspect': ['./node_modules/@sparticuz/chromium-min/**/*'],
-    '/api/automation/run': ['./node_modules/@sparticuz/chromium-min/**/*'],
+    '/api/automation/inspect': ['./node_modules/@sparticuz/chromium/**/*'],
+    '/api/automation/run': ['./node_modules/@sparticuz/chromium/**/*'],
   },
 };
 
